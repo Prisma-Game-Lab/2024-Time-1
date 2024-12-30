528fecbc33bb092cfb0a0deb550261dd560ea952
Vagner Messias <vagnermessias.vmc@gmail.com>
2024-12-30 12:00:06 -0300

    Stamina System

--- Time_1/Assets/Scripts/Player/PlayerController.cs
+++ Time_1/Assets/Scripts/Player/PlayerController.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float runSpeed = 10f;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float runSpeed = 10f;
+    [SerializeField] private float currentStamina;
+    [SerializeField] private float maxStamina = 10f;
+    [SerializeField] private float staminaRegenRate = 0.5f;
+    [SerializeField] private float staminaDecreaseRate = 0.5f;
+    [SerializeField] private bool regenerating = false;
+    [SerializeField] private bool regenerated = true;
+
     private float activeSpeed = 0f;
 
     public Rigidbody2D rb;
@@ -15,17 +22,36 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         activeSpeed = moveSpeed;
+        currentStamina = maxStamina;
     }
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-        if(Input.GetKey(KeyCode.LeftShift))
+        if(Input.GetKey(KeyCode.LeftShift) && currentStamina > 0 && regenerated) //Running 
         {
             activeSpeed = runSpeed;
+            currentStamina -= staminaDecreaseRate * Time.deltaTime;
+            if(currentStamina < 0)
+            {
+                regenerating = true;
+                regenerated = false;
+            }
         }
-        else { activeSpeed = moveSpeed;}
+        else //Walking
+        { 
+            activeSpeed = moveSpeed;
+            if(currentStamina < maxStamina - 0.01 && regenerating == true)
+            {
+                currentStamina += staminaRegenRate * Time.deltaTime;
+            }
+            else if (currentStamina >= maxStamina - 0.01 && regenerating == true)
+            {
+                regenerating = false;
+                regenerated = true;
+            }
+        }
 
     }
 
