45c4562beada60ec5a42a07d86b9c4784a4e12fa
Lucas Thurler <lucasthurlergoncalves@gmail.com>
2025-01-30 12:29:47 -0300

    temos som no menu do game!

--- Time_1/Assets/Scripts/UI/ExitButton.cs
+++ Time_1/Assets/Scripts/UI/ExitButton.cs
@@ -3,6 +3,18 @@ using UnityEngine;
 
 public class ExitButton : MonoBehaviour
 {
+    AudioManager audioManager;
+
+    public void start()
+    {
+        audioManager = AudioManager.instance;
+        if (audioManager == null)
+        {
+            Debug.LogError("No AudioManager found in scene.");
+        }
+        audioManager.StopSound("Theme");
+    }
+
     public void doExitGame()
     {
         Application.Quit();
@@ -10,6 +22,8 @@ public class ExitButton : MonoBehaviour
 
     public void RestartGame(string SceneToGo)
     {
+        //delay de 2 segundos para iniciar o jogo
+        Invoke("PlayStartGameSound", 3f);
         SceneManager.LoadScene(SceneToGo);
     }
 }
